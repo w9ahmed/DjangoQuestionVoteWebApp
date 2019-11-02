5b08d20ccf81ee27a4d6c03cdbbc9c4d2fb8b478
Ahmed SAMI MOHAMED <asyn.nile@gmail.com>
2019-11-02 21:56:48 +0100

    More features to admin page

--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
