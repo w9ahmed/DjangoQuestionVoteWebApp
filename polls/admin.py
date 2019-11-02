6ae5096a347d0ce7f72bd96caed7b770e0bc792f
Ahmed SAMI MOHAMED <asyn.nile@gmail.com>
2019-11-02 21:47:58 +0100

    Better admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from polls import models
+from polls.models import Question, Choice
 
-admin.site.register([models.Question, models.Choice])
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register([models.Question, models.Choice])
